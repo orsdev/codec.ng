e81bc8688926ff67f4afd304e79612e3c08fda39
phiiileo <emmanuelboluwajib1@gmail.com>
2019-12-17 08:23:47 +0100

    update

--- js/start_course.js
+++ js/start_course.js
@@ -1,42 +1,8 @@
 $(document).ready(function () {
 
-  //mobile navBar toggle
-  $("#hamb").click(function () {
-    let sidebar = $("#mobile");
-    $(sidebar).toggleClass("show_sidebar");
-    let sidebard = $(sidebar).css("left");
-    if (sidebard === "-500px") {
-      $(this).removeClass("fa-bars").addClass("fa-times")
-    } else {
-      $(this).removeClass("fa-times").addClass("fa-bars")
-    }
-  })
-
-  //mobile search function
-  $("#fsearch").click(function () {
-    $(".search").toggle(500)
-  })
-
-  // header courses 
-
-  let courses = $(".desktop ul li");
-  let courses_items = $(".navMenu div")
-  for (let i = 0; i < courses.length; i++) {
-    $(courses[i]).click(function () {
-      if ($(courses_items[i]).css("display") === "none") {
-        $(courses_items).css("display", "none");
-        $(courses_items[i]).css("display", "flex");
-
-      }
-      else {
-        $(courses_items[i]).css("display", "none")
-      }
-    })
-  }
-
   //sidebar 
   let closeButton = $(".close");
-  let closeIcon = $(".fa-plus");
+  let closeIcon = $(".plus");
   let sidebar_ul = $("#container");
   let sidebarBullet = $("#container li")
   let menuList = $(".menuList");
@@ -47,35 +13,34 @@ $(document).ready(function () {
   //button collapse events
   $(closeButton).click(() => {
     if (init1) {
-      (sidebar_ul).css("width", "70%");
-      menuList.css("display", "block");
-      $(closeIcon).addClass("fa-times").removeClass("fa-plus");
+      (sidebar_ul).css("width", "400px");
+      menuList.css("display", "flex");
       $(desktop_side).toggle();
-      $(header).css("width", "70%")
+      $(header).css("width", "500px");
+      $(closeIcon).css("transform", "rotate(130deg)")
       return init1 = false;
     }
     else {
-      (sidebar_ul).css("width", "70px");
-      menuList.css("display","none");
-      $(closeIcon).addClass("fa-plus").removeClass("fa-times");
+      (sidebar_ul).css("width", "60px");
+      menuList.css("display", "none");
       desktop_side.toggle();
-      $(header).css("width", "100px")
-
+      $(header).css("width", "150px");
+      $(closeIcon).css("transform", "rotate(180deg)")
       return init1 = true;
 
     }
 
   })
 
-  //menuList hover effect
+  // menuList hover effect
   for (let i = 0; i < sidebarBullet.length; i++) {
     $(sidebarBullet[i]).mouseenter(() => {
-        (sidebar_ul).css("width", "70%");
-        $(menuList[i]).css("display", "block")
-      
+        (sidebar_ul).css("width", "400px");
+        $(menuList[i]).css("display", "flex")
+
     })
     $(sidebarBullet[i]).mouseleave(() => {
-        (sidebar_ul).css("width", "70px");
+        (sidebar_ul).css("width", "60px");
         $(menuList).css("display", "none")
 
     })
