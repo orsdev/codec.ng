fd2b2981d39cf3f81a24d5668b60bf6c97609b2f
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-01-13 20:17:58 +0100

    footer and mobile nav created

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,28 @@
-// SIDEBAR TOGGLE
+function openNav(){
+ document.querySelector(".mobile__sidebar-bg").style.width = "300px";
+ document.querySelector(".mobile__sidebar").style.width = "100%";
+ document.querySelector(".mobile__sidebar-bg").style.transform = "translate(0)";
+}
+
+function closeNav(e){
+
+ if(e.target.classList.contains('mobile__sidebar')) {
+ document.querySelector(".mobile__sidebar-bg").style.width = "0";
+ document.querySelector(".mobile__sidebar").style.width = "0";
+ document.querySelector(".mobile__sidebar-bg").style.transform = "translate(-500px)";
+ }
+}
+
+//events
+function eventListener() {
+ const open = document.querySelector('.mobile-nav__toggle');
+ const close =  document.querySelector(".mobile__sidebar");
+
+
+ open.addEventListener('click' , openNav);
+ close.addEventListener('click' , closeNav);
+
+}
+
+//execute function
+eventListener();
