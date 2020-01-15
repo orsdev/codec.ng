5258bcd98b04106b13611c559e1bb76b878f42db
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-01-15 15:55:21 +0100

    all pages created

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,71 +1,114 @@
 
+
 // SIDEBAR TOGGLE
-function openNav(){
+function openNav() {
  document.querySelector(".mobile__sidebar-bg").style.width = "295px";
  document.querySelector(".mobile__sidebar").style.width = "100%";
  document.querySelector(".mobile__sidebar-bg").style.transform = "translate(0)";
 }
 
-function closeNav(e){
- if(e.target.classList.contains('mobile__sidebar')) {
- document.querySelector(".mobile__sidebar-bg").style.width = "0";
- document.querySelector(".mobile__sidebar").style.width = "0";
- document.querySelector(".mobile__sidebar-bg").style.transform = "translate(-500px)";
- }
+function closeNav(e) {
+
+  document.querySelector(".mobile__sidebar-bg").style.width = "0";
+  document.querySelector(".mobile__sidebar").style.width = "0";
+  document.querySelector(".mobile__sidebar-bg").style.transform = "translate(-500px)";
+ 
 }
 
- /********************************************************************* */
+/********************************************************************* */
 
- // SHOW ACTIVE PAGE
- function showPages(e) {
+function showPage(e) {
+ let target = e.target;
+ //get element
+ const show = document.querySelectorAll('.show');
+ let borderClass = document.querySelectorAll('.active-list');
 
-  let target = e.target;
+ //loop through element and remove classes
+ borderClass.forEach((elem) => {
+  elem.classList.remove('active-list');
+ });
 
-  //select dom elements
+
+ if (show) {
+  //loop through element and remove classes
+  show.forEach((elem) => {
+   elem.classList.remove('show');
+  });
+
+ }
+
+ if (target.classList.contains('show-perf')) {
+  //get dom elements
+  let overview = document.querySelector('.overview');
+  let performance = document.querySelector('.performance');
+  let showBorder = document.querySelector('.showBorder');
+
+  //add classes to element
+  overview.classList.add('show');
+  performance.classList.add('show');
+  showBorder.classList.add('active-list');
+ }
+
+ if (target.classList.contains('show-courses')) {
+  //get dom elements
   let courses = document.querySelector('.courses');
-  let activeClass = document.querySelectorAll('.active-list');
-  let communication = document.querySelector('.communication');
+  let courseBorder = document.querySelector('.courseBorder');
 
-    //loop & remove classes
-    activeClass.forEach((elem) => {
-     elem.classList.remove('active-list');
-    });
-
-    if(target.classList.contains('show-courses')) {
-     //select dom element
-     let coursesActive = document.querySelector('.courses-active');
-  
-     //add class to element
-     coursesActive.classList.add('active-list');
-  
-     courses.style.display = "block";
-     communication.style.display = "none";
-    }
-
-  if(target.classList.contains('show-comms')) {
-   //select dom element
-   let showActive = document.querySelector('.comms-active');
-
-   //add class to element
-   showActive.classList.add('active-list');
-
-   courses.style.display = "none";
-   communication.style.display = "flex";
-  }
-
+  //add classes to element
+  courses.classList.add('show');
+  courseBorder.classList.add('active-list');
  }
 
-//events
-function eventListener() {
- const open = document.querySelector('.mobile-nav__toggle');
- const close =  document.querySelector(".mobile__sidebar");
- let wrapper = document.querySelector('.wrapper');
+ if (target.classList.contains('show-comms')) {
+  //get dom elements
+  let communicate = document.querySelector('.communicate');
+  let comms = document.querySelector('.communication');
+  let commsBorder = document.querySelector('.commsBorder');
 
- open.addEventListener('click' , openNav);
- close.addEventListener('click' , closeNav);
- wrapper.addEventListener('click', showPages);
+  //add classes to element
+  comms.classList.add('show');
+  communicate.classList.add('show');
+  commsBorder.classList.add('active-list');
+ }
+
+ if (target.classList.contains('show-tools')) {
+  //get dom elements
+  let tools = document.querySelector('.tools');
+  let toolsBorder = document.querySelector('.toolsBorder');
+
+  //add classes to element
+  tools.classList.add('show');
+  toolsBorder.classList.add('active-list');
+ }
+
+ if (target.classList.contains('show-resource')) {
+  //get dom elements
+  let resource = document.querySelector('.resource');
+  let resourceBorder = document.querySelector('.resourceBorder');
+
+  //add classes to element
+  resource.classList.add('show');
+  resourceBorder.classList.add('active-list');
+ }
 
 }
 
-//execute function
-eventListener();
+function listener() {
+ //get dom element
+ const sidebar = document.querySelector('.sidebar');
+ const mobileSidebar = document.querySelector('.mobile__sidebar-list');
+ const open = document.querySelector('.mobile-nav__toggle');
+ const close = document.querySelector(".mobile__sidebar");
+
+ //add event to element
+ open.addEventListener('click', openNav);
+ close.addEventListener('click', closeNav);
+ sidebar.addEventListener('click', showPage);
+ mobileSidebar.addEventListener('click', function(e){
+  showPage(e);
+  closeNav();
+ });
+}
+
+//call function
+listener();
